52a86f17bbf8abeb7fce4a134d326cfaf1d30605
Geigle <gregortheodor.geigle@stud.tu-darmstadt.de>
2021-05-01 16:41:59 +0200

    updated requirements, added wandb support, more logging, save best mode explicitly

--- run.py
+++ run.py
@@ -14,15 +14,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def main(config):
+    if config.get("wandb_project", ""):
+        os.environ["WANDB_PROJECT"] = config["wandb_project"]
     if config.get("do_train", True):
         train(config)
     else:
         test(config)
 
+
 def train(config):
-    output_dir = os.path.join(config["output_dir"], f"{datetime.now().strftime('%Y-%m-%d_%H-%M')}")
+    logger.info(config)
+    task_folder = f"{config.get('task_name', '')}{config['task']}_{datetime.now().strftime('%Y-%m-%d_%H-%M')}"
+    output_dir = os.path.join(config["output_dir"], task_folder)
     os.makedirs(output_dir, exist_ok=True)
+    logger.info(f"Saving results in {output_dir}")
     yaml.dump(config, open(os.path.join(output_dir, "train_config.yaml"), "w"))
 
     model_config = XLMRobertaConfig.from_pretrained(config.get("model", "xlm-roberta-base"), num_labels=1)
@@ -30,12 +37,12 @@ def train(config):
 
     task = config["task"]
     assert task == "qe_da" or task == "qe_hter"
-
     model.add_adapter(task)
     model.add_classification_head(task, num_labels=1)
 
     train_config = config["train"]
     train_lang1, train_lang2 = train_config["pair"]
+    logger.info(f"Training for {task} {train_lang1}-{train_lang2}")
     model.load_adapter(f"{train_lang1}/wiki@ukp")
     model.load_adapter(f"{train_lang2}/wiki@ukp")
 
@@ -56,7 +63,9 @@ def train(config):
         fp16=train_config.get("amp", True),
         eval_steps=train_config.get("eval_steps", 250),
         evaluation_strategy="steps",
-        load_best_model_at_end=True
+        load_best_model_at_end=True,
+        run_name=task_folder,
+        report_to=train_config.get("report_to", "all")
     )
 
     trainer = Trainer(
@@ -69,24 +78,31 @@ def train(config):
         do_save_full_model=False
     )
     trainer.train()
-    test(config, model, output_dir)
+    best_checkpoint = os.path.join(output_dir, "best_checkpoint", task)
+    os.makedirs(best_checkpoint, exist_ok=True)
+    model.save_adapter(best_checkpoint, task)
+    test(config, model, task_folder)
 
-def test(config, model=None, output_dir=None):
+
+def test(config, model=None, task_folder=None):
     task = config["task"]
     assert task == "qe_da" or task == "qe_hter"
     if not model:
+        logger.info(f"Loading task adapter from {config['adapter_path']}")
         model_config = XLMRobertaConfig.from_pretrained(config.get("model", "xlm-roberta-base"), num_labels=1)
         model = XLMRobertaModelWithHeads.from_pretrained(config.get("model", "xlm-roberta-base"), config=model_config)
         model.load_adapter(config["adapter_path"], model_name=task)
-    if not output_dir:
-        output_dir = os.path.join(config["output_dir"], f"{datetime.now().strftime('%Y-%m-%d_%H-%M')}")
+    if not task_folder:
+        task_folder = f"{config.get('task_name', '')}{config['task']}{datetime.now().strftime('%Y-%m-%d_%H-%M')}"
+        output_dir = os.path.join(config["output_dir"], task_folder)
         os.makedirs(output_dir, exist_ok=True)
+        logger.info(f"Saving results in {output_dir}")
     yaml.dump(config, open(os.path.join(output_dir, "test_config.yaml"), "w"))
     for pair in config["test"]["pairs"]:
         lang1 = pair[0]
         lang2 = pair[1]
         dataset = load_data(lang1, lang2, task, config)
-
+        logger.info(f"Evaluation results for {task} {lang1}-{lang2}")
         model.load_adapter(f"{lang1}/wiki@ukp")
         model.load_adapter(f"{lang2}/wiki@ukp")
 
@@ -97,16 +113,17 @@ def test(config, model=None, output_dir=None):
             args=TrainingArguments(output_dir=output_dir, remove_unused_columns=False, per_device_eval_batch_size=config["test"]["batchsize"]),
             eval_dataset=dataset["test"],
             compute_metrics=compute_pearson,
+            run_name=task_folder
         )
         evaluation = eval_trainer.evaluate(metric_key_prefix="test")
         logger.info(evaluation)
         json.dump(evaluation, open(os.path.join(output_dir, f"da_{lang1}_{lang2}.json", "w")))
 
+
 def load_data(lang1, lang2, task, config):
     tokenizer = XLMRobertaTokenizer.from_pretrained(config.get("model", "xlm-roberta-base"))
     if config.get("model", "xlm-roberta-base") != "xlm-roberta-base":
         logger.warning("encode_batch not implemented for non-xlmr models!")
-
 
     if task == "qe_da":
         dataset = load_dataset("csv", delimiter="\t", quoting=3, data_files={
@@ -134,7 +151,6 @@ def load_data(lang1, lang2, task, config):
         test = Dataset.from_dict({"original": test_src, "translation": test_mt, "label": test_hter}, split="test")
         dataset = DatasetDict({"train": train, "dev": dev, "test": test})
 
-
     def encode_batch(batch):
         """Encodes a batch of input data using the model tokenizer."""
         sen1 = tokenizer(batch["original"], max_length=config.get("max_seq_len", 50), truncation=True, padding="max_length")
@@ -155,8 +171,10 @@ def load_data(lang1, lang2, task, config):
 
     return dataset
 
+
 def compute_pearson(p: EvalPrediction):
     return {"pearson": pearsonr(p.predictions.reshape(-1), p.label_ids)[0]}
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
