4a2d8b0e4c350779de2f895e55b5b3ddd124d720
Geigle <gregortheodor.geigle@stud.tu-darmstadt.de>
2021-05-04 15:33:01 +0200

    added training with multiple language pairs

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
 import argparse
+import collections
 import json
 import os
 from datetime import datetime
+from typing import Union, List, Optional
 
-import yaml
 import torch
-from transformers import XLMRobertaModelWithHeads, XLMRobertaConfig, XLMRobertaTokenizer, TrainingArguments, Trainer, EvalPrediction
+import yaml
+from torch.utils.data import DataLoader, Sampler
+from transformers import XLMRobertaModelWithHeads, XLMRobertaConfig, XLMRobertaTokenizer, TrainingArguments, Trainer, EvalPrediction, TrainerCallback
 import numpy as np
 from scipy.stats import pearsonr
 import transformers.adapters.composition as ac
 from datasets import load_dataset, Dataset, DatasetDict
 import logging
+
+from transformers.trainer_pt_utils import nested_concat, DistributedTensorGatherer, SequentialDistributedSampler
+from transformers.trainer_utils import PredictionOutput, denumpify_detensorize
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -47,30 +53,26 @@ def train(config):
         model.add_classification_head(task, num_labels=1)
 
     train_config = config["train"]
-    train_lang1, train_lang2 = train_config["pair"]
-    logger.info(f"Training for {task} {train_lang1}-{train_lang2}")
-    model.load_adapter(f"{train_lang1}/wiki@ukp")
-    model.load_adapter(f"{train_lang2}/wiki@ukp")
+    is_multipair = not isinstance(train_config["pair"][0], str)
+    logger.info(f"Training for {task} {train_config['pair']}")
+    pairs = train_config["pair"] if is_multipair else [train_config["pair"]]
+    for train_lang1, train_lang2 in pairs:
+        model.load_adapter(f"{train_lang1}/wiki@ukp")
+        model.load_adapter(f"{train_lang2}/wiki@ukp")
+
     if config.get("split", True):
         model.train_adapter([task+"_original", task+"_translation"])
     else:
         model.train_adapter([task])
-    skip_layer = [11] if config.get("madx2", False) else []
-    if config.get("split", True):
-        setup = [ac.Split(train_lang1, train_lang2, split_index=config.get("max_seq_len", 50)),
-                 ac.Split(task+"_original", task+"_translation", split_index=config.get("max_seq_len", 50))]
-    else:
-        setup = [ac.Split(train_lang1, train_lang2, split_index=config.get("max_seq_len", 50)), task]
-    model.set_active_adapters(setup, skip_layers=skip_layer)
 
-    dataset = load_data(train_lang1, train_lang2, task, config)
+    dataset = load_data(train_config["pair"], task, config)
 
     training_args = TrainingArguments(
         learning_rate=train_config.get("learning_rate", 0.0001),
         num_train_epochs=train_config["epochs"],
         max_steps=train_config.get("max_steps", -1),
         per_device_train_batch_size=train_config.get("train_batchsize", 16),
-        per_device_eval_batch_size=train_config.get("dev_batchsize", 32),
+        per_device_eval_batch_size=train_config.get("eval_batchsize", 32),
         logging_steps=train_config.get("logging_steps", 10),
         output_dir=output_dir,
         overwrite_output_dir=True,
@@ -79,21 +81,46 @@ def train(config):
         eval_steps=train_config.get("eval_steps", 250),
         evaluation_strategy="steps",
         load_best_model_at_end=True,
+        metric_for_best_model="pearson",
         save_total_limit=train_config.get("save_total_limit", None),
         run_name=task_folder,
         report_to=config.get("report_to", "all"),
         skip_memory_metrics=config.get("skip_memory_metrics", True)
     )
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=dataset["train"],
-        eval_dataset=dataset["dev"],
-        compute_metrics=compute_pearson,
-        do_save_adapters=True,
-        do_save_full_model=False
-    )
+
+
+    skip_layer = [11] if config.get("madx2", False) else []
+    if is_multipair:
+        task_adapter = ac.Split(task+"_original", task+"_translation", split_index=config.get("max_seq_len", 50))\
+            if config.get("split", True) else task
+        trainer = CustomTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=dataset["train"],
+            eval_dataset=dataset["dev"],
+            compute_metrics=compute_pearson,
+            do_save_adapters=True,
+            do_save_full_model=False
+        )
+        trainer.add_callback(AdapterLangCallback(pairs, task_adapter, config.get("max_seq_len", 50), skip_layer))
+    else:
+        train_lang1, train_lang2 = pairs[0]
+        if config.get("split", True):
+            setup = [ac.Split(train_lang1, train_lang2, split_index=config.get("max_seq_len", 50)),
+                     ac.Split(task+"_original", task+"_translation", split_index=config.get("max_seq_len", 50))]
+        else:
+            setup = [ac.Split(train_lang1, train_lang2, split_index=config.get("max_seq_len", 50)), task]
+        model.set_active_adapters(setup, skip_layers=skip_layer)
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=dataset["train"],
+            eval_dataset=dataset["dev"],
+            compute_metrics=compute_pearson,
+            do_save_adapters=True,
+            do_save_full_model=False
+        )
     trainer.train()
     if config.get("split", True):
         best_checkpoint = os.path.join(output_dir, "best_checkpoint")
@@ -130,9 +157,8 @@ def test(config, model=None, task_folder=None):
     yaml.dump(config, open(os.path.join(output_dir, "test_config.yaml"), "w"))
     results = {"dev": [], "test": [], "task": task}
     for pair in config["test"]["pairs"]:
-        lang1 = pair[0]
-        lang2 = pair[1]
-        dataset = load_data(lang1, lang2, task, config)
+        lang1, lang2 = pair
+        dataset = load_data(pair, task, config)
         logger.info(f"Evaluation results for {task} {lang1}-{lang2}")
         model.load_adapter(f"{lang1}/wiki@ukp")
         model.load_adapter(f"{lang2}/wiki@ukp")
@@ -176,16 +202,18 @@ def test(config, model=None, task_folder=None):
     json.dump(results, open(os.path.join(output_dir, f"evaluation_{task}.json"), "w"), indent=2)
 
 
-def load_data(lang1, lang2, task, config):
+def load_data(lang_pairs, task, config):
+    if isinstance(lang_pairs[0], str):
+        lang_pairs = [lang_pairs]
     tokenizer = XLMRobertaTokenizer.from_pretrained(config.get("model", "xlm-roberta-base"))
     if config.get("model", "xlm-roberta-base") != "xlm-roberta-base":
         logger.warning("encode_batch not implemented for non-xlmr models!")
 
     if task == "qe_da":
         dataset = load_dataset("csv", delimiter="\t", quoting=3, data_files={
-            "train": f"data/data/direct-assessments/train/{lang1}-{lang2}-train/train.{lang1}{lang2}.df.short.tsv",
-            "test": f"data/data/direct-assessments/test/{lang1}-{lang2}/test20.{lang1}{lang2}.df.short.tsv",
-            "dev": f"data/data/direct-assessments/dev/{lang1}-{lang2}-dev/dev.{lang1}{lang2}.df.short.tsv"
+            "train": [f"data/data/direct-assessments/train/{lang1}-{lang2}-train/train.{lang1}{lang2}.df.short.tsv" for (lang1, lang2) in lang_pairs],
+            "test": [f"data/data/direct-assessments/test/{lang1}-{lang2}/test20.{lang1}{lang2}.df.short.tsv" for (lang1, lang2) in lang_pairs],
+            "dev": [f"data/data/direct-assessments/dev/{lang1}-{lang2}-dev/dev.{lang1}{lang2}.df.short.tsv" for (lang1, lang2) in lang_pairs]
         })
         # The transformers model expects the target class column to be named "labels"
         dataset = dataset.rename_column("z_mean", "label")
@@ -193,15 +221,17 @@ def load_data(lang1, lang2, task, config):
     if task == "qe_hter":
         def read_f(f, dt):
             return [dt(l.strip()) for l in open(f, encoding="utf-8").readlines()]
-        train_hter = read_f(f"data/data/post-editing/train/{lang1}-{lang2}-train/train.hter", float)
-        dev_hter = read_f(f"data/data/post-editing/dev/{lang1}-{lang2}-dev/dev.hter", float)
-        test_hter = read_f(f"data/data/post-editing/test/{lang1}-{lang2}-test20/test20.hter", float)
-        train_src = read_f(f"data/data/post-editing/train/{lang1}-{lang2}-train/train.src", str)
-        dev_src = read_f(f"data/data/post-editing/dev/{lang1}-{lang2}-dev/dev.src", str)
-        test_src = read_f(f"data/data/post-editing/test/{lang1}-{lang2}-test20/test20.src", str)
-        train_mt = read_f(f"data/data/post-editing/train/{lang1}-{lang2}-train/train.mt", str)
-        dev_mt = read_f(f"data/data/post-editing/dev/{lang1}-{lang2}-dev/dev.mt", str)
-        test_mt = read_f(f"data/data/post-editing/test/{lang1}-{lang2}-test20/test20.mt", str)
+        train_hter, dev_hter, test_hter, train_src, dev_src, test_src, train_mt, dev_mt, test_mt = [], [], [], [], [], [], [], [], []
+        for (lang1, lang2) in lang_pairs:
+            train_hter.extend(read_f(f"data/data/post-editing/train/{lang1}-{lang2}-train/train.hter", float))
+            dev_hter.extend(read_f(f"data/data/post-editing/dev/{lang1}-{lang2}-dev/dev.hter", float))
+            test_hter.extend(read_f(f"data/data/post-editing/test/{lang1}-{lang2}-test20/test20.hter", float))
+            train_src.extend(read_f(f"data/data/post-editing/train/{lang1}-{lang2}-train/train.src", str))
+            dev_src.extend(read_f(f"data/data/post-editing/dev/{lang1}-{lang2}-dev/dev.src", str))
+            test_src.extend(read_f(f"data/data/post-editing/test/{lang1}-{lang2}-test20/test20.src", str))
+            train_mt.extend(read_f(f"data/data/post-editing/train/{lang1}-{lang2}-train/train.mt", str))
+            dev_mt.extend(read_f(f"data/data/post-editing/dev/{lang1}-{lang2}-dev/dev.mt", str))
+            test_mt.extend(read_f(f"data/data/post-editing/test/{lang1}-{lang2}-test20/test20.mt", str))
         train = Dataset.from_dict({"original": train_src, "translation": train_mt, "label": train_hter}, split="train")
         dev = Dataset.from_dict({"original": dev_src, "translation": dev_mt, "label": dev_hter}, split="dev")
         test = Dataset.from_dict({"original": test_src, "translation": test_mt, "label": test_hter}, split="test")
@@ -236,8 +266,197 @@ def load_data(lang1, lang2, task, config):
 
 
 def compute_pearson(p: EvalPrediction):
-    return {"pearson": pearsonr(p.predictions.reshape(-1), p.label_ids)[0]}
+    if len(p.predictions) == 1000: #hard coded
+        return {"pearson": pearsonr(p.predictions.reshape(-1), p.label_ids)[0]}
+    else:
+        predictions = p.predictions.reshape(-1)
+        all_r = []
+        for i in range(0, len(predictions), 1000):
+            all_r.append(pearsonr(predictions[i:i+1000], p.label_ids[i:i+1000])[0])
+        return {"pearson": np.mean(all_r)}
 
+
+class CustomTrainer(Trainer):
+    def get_train_dataloader(self):
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+        train_sampler = MultiSampler(self.train_dataset, self.args.train_batch_size)
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.args.train_batch_size,
+            sampler=train_sampler,
+            collate_fn=self.data_collator,
+            drop_last=self.args.dataloader_drop_last,
+            num_workers=self.args.dataloader_num_workers,
+            pin_memory=self.args.dataloader_pin_memory,
+        )
+
+    def prediction_loop(
+            self,
+            dataloader: DataLoader,
+            description: str,
+            prediction_loss_only: Optional[bool] = None,
+            ignore_keys: Optional[List[str]] = None,
+            metric_key_prefix: str = "eval",
+    ) -> PredictionOutput:
+        """
+        Prediction/evaluation loop, shared by :obj:`Trainer.evaluate()` and :obj:`Trainer.predict()`.
+
+        Works both with or without labels.
+        """
+        if not isinstance(dataloader.dataset, collections.abc.Sized):
+            raise ValueError("dataset must implement __len__")
+        prediction_loss_only = (
+            prediction_loss_only if prediction_loss_only is not None else self.args.prediction_loss_only
+        )
+
+        if self.args.deepspeed and not self.args.do_train:
+            # no harm, but flagging to the user that deepspeed config is ignored for eval
+            # flagging only for when --do_train wasn't passed as only then it's redundant
+            logger.info("Detected the deepspeed argument but it will not be used for evaluation")
+
+        model = self._wrap_model(self.model, training=False)
+
+        # if full fp16 is wanted on eval and this ``evaluation`` or ``predict`` isn't called while
+        # ``train`` is running, half it first and then put on device
+        if not self.is_in_train and self.args.fp16_full_eval:
+            model = model.half().to(self.args.device)
+
+        batch_size = dataloader.batch_size
+        num_examples = self.num_examples(dataloader)
+        logger.info(f"***** Running {description} *****")
+        logger.info(f"  Num examples = {num_examples}")
+        logger.info(f"  Batch size = {batch_size}")
+        losses_host: torch.Tensor = None
+        preds_host: Union[torch.Tensor, List[torch.Tensor]] = None
+        labels_host: Union[torch.Tensor, List[torch.Tensor]] = None
+
+        world_size = max(1, self.args.world_size)
+
+        eval_losses_gatherer = DistributedTensorGatherer(world_size, num_examples, make_multiple_of=batch_size)
+        if not prediction_loss_only:
+            # The actual number of eval_sample can be greater than num_examples in distributed settings (when we pass
+            # a batch size to the sampler)
+            make_multiple_of = None
+            if hasattr(dataloader, "sampler") and isinstance(dataloader.sampler, SequentialDistributedSampler):
+                make_multiple_of = dataloader.sampler.batch_size
+            preds_gatherer = DistributedTensorGatherer(world_size, num_examples, make_multiple_of=make_multiple_of)
+            labels_gatherer = DistributedTensorGatherer(world_size, num_examples, make_multiple_of=make_multiple_of)
+
+        model.eval()
+
+        if self.args.past_index >= 0:
+            self._past = None
+
+        self.callback_handler.eval_dataloader = dataloader
+
+        for step, inputs in enumerate(dataloader):
+            ### THIS IS A DIRTY HACK
+            if (step * batch_size)%1000 == 0:
+                self.callback_handler.callbacks[-1].next_test_adapter(model)
+            loss, logits, labels = self.prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
+            if loss is not None:
+                losses = loss.repeat(batch_size)
+                losses_host = losses if losses_host is None else torch.cat((losses_host, losses), dim=0)
+            if logits is not None:
+                preds_host = logits if preds_host is None else nested_concat(preds_host, logits, padding_index=-100)
+            if labels is not None:
+                labels_host = labels if labels_host is None else nested_concat(labels_host, labels, padding_index=-100)
+            self.control = self.callback_handler.on_prediction_step(self.args, self.state, self.control)
+
+            # Gather all tensors and put them back on the CPU if we have done enough accumulation steps.
+            if self.args.eval_accumulation_steps is not None and (step + 1) % self.args.eval_accumulation_steps == 0:
+                eval_losses_gatherer.add_arrays(self._gather_and_numpify(losses_host, "eval_losses"))
+                if not prediction_loss_only:
+                    preds_gatherer.add_arrays(self._gather_and_numpify(preds_host, "eval_preds"))
+                    labels_gatherer.add_arrays(self._gather_and_numpify(labels_host, "eval_label_ids"))
+
+                # Set back to None to begin a new accumulation
+                losses_host, preds_host, labels_host = None, None, None
+
+        if self.args.past_index and hasattr(self, "_past"):
+            # Clean the state at the end of the evaluation loop
+            delattr(self, "_past")
+
+        # Gather all remaining tensors and put them back on the CPU
+        eval_losses_gatherer.add_arrays(self._gather_and_numpify(losses_host, "eval_losses"))
+        if not prediction_loss_only:
+            preds_gatherer.add_arrays(self._gather_and_numpify(preds_host, "eval_preds"))
+            labels_gatherer.add_arrays(self._gather_and_numpify(labels_host, "eval_label_ids"))
+
+        eval_loss = eval_losses_gatherer.finalize()
+        preds = preds_gatherer.finalize() if not prediction_loss_only else None
+        label_ids = labels_gatherer.finalize() if not prediction_loss_only else None
+
+        if self.compute_metrics is not None and preds is not None and label_ids is not None:
+            metrics = self.compute_metrics(EvalPrediction(predictions=preds, label_ids=label_ids))
+        else:
+            metrics = {}
+
+        # To be JSON-serializable, we need to remove numpy types or zero-d tensors
+        metrics = denumpify_detensorize(metrics)
+
+        if eval_loss is not None:
+            metrics[f"{metric_key_prefix}_loss"] = eval_loss.mean().item()
+
+        # Prefix all keys with metric_key_prefix + '_'
+        for key in list(metrics.keys()):
+            if not key.startswith(f"{metric_key_prefix}_"):
+                metrics[f"{metric_key_prefix}_{key}"] = metrics.pop(key)
+
+        return PredictionOutput(predictions=preds, label_ids=label_ids, metrics=metrics)
+
+
+class AdapterLangCallback(TrainerCallback):
+    def __init__(self, pairs, task_adapters, split_index, skip_layers):
+        self.pairs = pairs
+        self.task_adapters = task_adapters
+        self.split_index = split_index
+        self.skip_layers = skip_layers
+        self.train_idx = 0
+        self.test_idx = 0
+
+    def on_step_begin(self, args: TrainingArguments, state, control, **kwargs):
+        """
+        Event called at the beginning of a training step. If using gradient accumulation, one training step might take
+        several inputs.
+        """
+        model = kwargs["model"]
+        train_lang1, train_lang2 = self.pairs[self.train_idx]
+        setup = [ac.Split(train_lang1, train_lang2, split_index=self.split_index), self.task_adapters]
+        model.set_active_adapters(setup, skip_layers=self.skip_layers)
+        self.train_idx += 1
+        self.train_idx = self.train_idx % len(self.pairs)
+
+    def next_test_adapter(self, model):
+        test_lang1, test_lang2 = self.pairs[self.test_idx]
+        setup = [ac.Split(test_lang1, test_lang2, split_index=self.split_index), self.task_adapters]
+        model.set_active_adapters(setup, skip_layers=self.skip_layers)
+        self.test_idx += 1
+        self.test_idx = self.test_idx % len(self.pairs)
+
+
+class MultiSampler(Sampler):
+    def __init__(self, data_source, batchsize):
+        self.data_source = data_source
+        self.batchsize = batchsize
+        self.max_rounds = len(self.data_source)//7000
+        self.max_get = self.batchsize*(7000//self.batchsize)
+
+    def __iter__(self):
+        permutation = np.random.permutation(range(7000))
+        idx = 0
+        round = 0
+        while idx < self.max_get:
+            for i in range(self.batchsize):
+                yield permutation[idx+i].item() + round*7000
+            round += 1
+            if round == self.max_rounds:
+                idx += self.batchsize
+                round = 0
+
+    def __len__(self) -> int:
+        return self.max_rounds * self.max_get
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
